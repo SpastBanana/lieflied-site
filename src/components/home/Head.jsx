8804f2b8ffec17348d932536ab1d2346e8b9fbff
Sebastiaan de Vries <sebastiaan@huizedevries.nl>
2025-08-16 22:38:41 +0200

    Homepage improvements

--- src/components/home/Head.jsx
+++ src/components/home/Head.jsx
@@ -6,15 +6,18 @@ export const Head = () => {
       <img
         src="/img/site/bg.png"
         alt="Banner"
-        className="w-full h-full absolute object-cover z-5 blur-xs saturate-150"
+        className="w-full h-full absolute object-cover z-5 blur-xs saturate-150 overflow-hidden"
       />
-      <div className="w-full h-full absolute z-10 bg-[#00000077] drop-shadow-none border-none"></div>
-      <div className="w-full h-full absolute z-20 flex items-center justify-evenly flex-col">
+      <div className="w-full h-full absolute z-10 bg-[#00000077] innershade"></div>
+      <div className="w-full h-full absolute z-20 flex items-center justify-center gap-20 flex-col">
         <img
           src="/img/site/Logo.jpeg"
           alt="Logo"
-          className="w-80 rounded-lg shadow"
+          className="w-90 rounded-lg shadow"
         />
+        <h2 className="h1 text-gold tracking-wider text-glow border-">
+          Kaartverkoop!
+        </h2>
       </div>
       <div className="absolute z-20 bottom-0 left-0 w-full h-40 flex items-center justify-center gap-30 text-4xl text-primary-foreground-inverted">
         <a
