5b8bef196e6ca68021b8562d748d4d58c9f36bcf
Sebastiaan de Vries <sebastiaan@huizedevries.nl>
2025-08-16 23:10:30 +0200

    Adapting navbar colors to green style

--- src/components/home/Nav.jsx
+++ src/components/home/Nav.jsx
@@ -3,11 +3,11 @@ import { BiUser } from "react-icons/bi";
 export const Nav = () => {
   return (
     <div className="h-14 w-screen bg-[#00000066] flex items-center justify-between text-graytext z-50 fixed">
-      <div className="ml-9 flex gap-5 t3 text-primary-foreground-inverted">
+      <div className="ml-9 flex gap-5 t3 text-white">
         <a href="#info">Info</a>
         <a href="#kaarten">Kaarten kopen</a>
       </div>
-      <div className="mr-9 t-ico3 text-primary-foreground-inverted">
+      <div className="mr-9 t-ico3 text-white">
         <a href="/login">
           <BiUser />
         </a>
