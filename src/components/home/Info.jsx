0aff13e171bcc2f6eac994a31c2efea075daf13f
Sebastiaan de Vries <sebastiaan@huizedevries.nl>
2025-08-16 23:06:25 +0200

    Info page adjustments

--- src/components/home/Info.jsx
+++ src/components/home/Info.jsx
@@ -4,14 +4,10 @@ export const Info = () => {
   return (
     <div
       id="info"
-      className="min-h-screen w-screen flex items-center justify-center flex-col gap-18"
+      className="min-h-screen w-screen flex items-center justify-center flex-col gap-18 bg-white"
     >
       <img src="/img/site/Ticket.png" alt="Ticket icon" className="w-40" />
-      <img
-        src="/img/site/Logo.jpeg"
-        alt="Logo Lief & Lied"
-        className="w-56 rounded-md shadow"
-      />
+      <img src="/img/site/Logo.jpeg" alt="Logo Lief & Lied" className="w-60" />
       <p className="t2 text-primary-foreground w-1/3 text-center">
         Bestel jouw kaarten via het formulier hieronder. Voor vragen over jouw
         kaarten mag je ons altijd mailen
