51c6a43b82aa429d34948456374d37cd663ed219
Sebastiaan de Vries <sebastiaan@huizedevries.nl>
2025-08-16 23:00:23 +0200

    Navbar adjustment

--- src/components/home/Nav.jsx
+++ src/components/home/Nav.jsx
@@ -2,7 +2,7 @@ import { BiUser } from "react-icons/bi";
 
 export const Nav = () => {
   return (
-    <div className="h-16 w-screen bg-[#00000066] flex items-center justify-between text-graytext z-50 fixed">
+    <div className="h-14 w-screen bg-[#00000066] flex items-center justify-between text-graytext z-50 fixed">
       <div className="ml-9 flex gap-5 t3 text-primary-foreground-inverted">
         <a href="#info">Info</a>
         <a href="#kaarten">Kaarten kopen</a>
