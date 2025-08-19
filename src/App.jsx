9583fdf7e721e45d2457c174557902c0f41bae58
Sebastiaan de Vries <sebastiaan@huizedevries.nl>
2025-08-19 18:29:05 +0200

    RESTORE POINT | Working frontend, empty backend logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
 import { Footer } from "./components/home/footer";
 import { Head } from "./components/home/Head";
 import { Info } from "./components/home/Info";
-import { LoginPopup } from "./components/home/LoginPopup";
 import { Nav } from "./components/home/Nav";
 import { Order } from "./components/home/Order";
+import { LoginPopup } from "./components/home/LoginPopup";
+import { useState } from "react";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
-
   return (
     <>
       <Nav setMenuOpen={setMenuOpen} />
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
-// main.jsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./app.css";
 import "./admin.css";
 import App from "./App.jsx";
-import Admin from "./Admin.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
